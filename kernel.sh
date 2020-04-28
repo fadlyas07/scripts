5bd1e15b26b2fa4ac265975c328ea7f53a6fc281
fadlyas07 <fadlyardhians@gmail.com>
2020-04-28 10:18:34 +0700

    USER -> HOST, HOST <- USER
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -32,8 +32,8 @@ export TELEGRAM_ID=$chat_id
 export TELEGRAM_TOKEN=$token
 export pack=$(pwd)/anykernel-3
 export product_name=GreenForce
-export KBUILD_BUILD_USER=$(git log --format='%H' -1)
-export KBUILD_BUILD_HOST=$(git log --format='%cn' -1)
+export KBUILD_BUILD_HOST=$(git log --format='%H' -1)
+export KBUILD_BUILD_USER=$(git log --format='%cn' -1)
 export kernel_img=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 build_start=$(date +"%s")
 
