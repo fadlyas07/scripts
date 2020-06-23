7ca8330721fd51f89e1af0fe670011957c794f7e
fadlyas07 <fadlyardhians@gmail.com>
2020-06-23 03:07:09 +0000

    build.local: fix build & more
    Change-Id: Ic66238e1edbd523ccf4198568a35d54ee022efff

--- build.local.sh
+++ build.local.sh
@@ -2,18 +2,27 @@
 # Ubuntu 18.04 LTS - Simple local kernel build script
 # Copyright (C) 2019, 2020, Raphielscape LLC (@raphielscape)
 # Copyright (C) 2019, 2020, Dicky Herlambang (@Nicklas373)
-# Copyright (C) 2019, 2020, Dhimas Bagus Prayoga (kry9ton)
+# Copyright (C) 2019, 2020, Dhimas Bagus Prayoga (@kry9ton)
 # Copyright (C) 2020, Muhammad Fadlyas (@fadlyas07)
 export ARCH=arm64
+export pack=$(pwd)/anykernel-3
+export github_name=$(git config user.name)
+export github_email=$(git config user.email)
 export parse_branch=$(git rev-parse --abbrev-ref HEAD)
 export kernel_img=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-export pack=$(pwd)/anykernel-3
 if ! [[ -e $(pwd)/anykernel-3 ]]; then
     git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 fi
 if ! [[ -e $(pwd)/gcc ]]; then
     git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r57 gcc
     git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r57 gcc32
+fi
+if [ -z $github_name ] && [ -z $github_email ]; then
+    echo -e "empty git config user.name & git config user.email"
+    read -p "Enter user.name: " NAME
+    read -p "Enter user.email: " EMAIL
+    git config --global user.name "$NAME"
+    git config --global user.email "$EMAIL"
 fi
 clear
 while true; do
@@ -24,20 +33,17 @@ while true; do
     echo -ne "\n(i) Please enter a choice[1-5]: "
     read choice
 if [ "$choice" = "1" ]; then
-   if [ -z $config ]; then
-       echo -e ""
-       sleep 2
-       echo -e "Please setup your defconfig name!"
-       echo -e ""
-       echo -e "Use command 'export config=<defconfig>', then run the script again."
-       echo -e ""
-       echo -e "(i) If your defconfig at vendor, use 'export config=vendor/<defconfig>'"
-       exit 1
-    fi
+    clear
     echo -e ""
+    echo -e "Enter your defconfig name"
+    read -p "Enter defconfig: " CONFIG
+    if [[ -z $CONFIG ]]; then
+        echo -e " You GaY plox, enter your defconfig name first!"
+        exit 1
+    fi
+    make O=out ARCH=arm64 $CONFIG > /dev/null
     PATH=$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH && \
-    make O=out ARCH=arm64 $config > /dev/null
-    make -j$(nproc) O=out \ ARCH=arm64 \ CROSS_COMPILE=aarch64-linux-android- \ CROSS_COMPILE_ARM32=arm-linux-androideabi- 2>&1| tee Log-$(TZ=Asia/Jakarta date +'%d%m%y').log
+    make -j$(nproc --all) O=out \ ARCH=arm64 \ CROSS_COMPILE=aarch64-linux-android- \ CROSS_COMPILE_ARM32=arm-linux-androideabi- 2>&1| tee Log-$(TZ=Asia/Jakarta date +'%d%m%y').log
     build_start=$(date +"%s")
     echo -e "\n#######################################################################"
     echo -e "(i) Build started at $(`date`)"
@@ -56,8 +62,8 @@ if [ "$choice" = "1" ]; then
     if ! [ -f $kernel_img ]; then
         build_end=$(date +"%s")
         build_diff=$(($build_end - $build_start))
-        grep -iE 'un|declare|not|empty|in file|waiting|crash|error|fail|fatal' "$(echo *.log)" &> "trimmed_log.txt"
-        echo -e "\n(!) Kernel compilation failed at $(($build_diff / 60)) minutes and $(($build_end % 60)) seconds, See build log to fix errors"
+        grep -iE 'not|empty|in file|waiting|crash|error|fail|fatal' "$(echo *.log)" &> "trimmed_log.txt"
+        echo -e "\n(!) Kernel compilation failed at $(($build_diff / 60)) minutes and $(($build_end % 60)) seconds."
         echo -e "#######################################################################"
         exit 1
     fi
@@ -69,20 +75,22 @@ if [ "$choice" = "1" ]; then
     echo -e "#######################################################################"
 fi
 if [ "$choice" = "2" ]; then
+    clear
     echo -e "\n#######################################################################"
     make O=out clean &>/dev/null
     make mrproper &>/dev/null
-    rm -rf out/*
+    rm -rf out/* temp trimm*.txt *.log
     echo -e "(i) Kernel source cleaned up."
     echo -e "#######################################################################"
 fi
 if [ "$choice" = "3" ]; then
+    clear
     echo -e "\n#######################################################################"
     cd $pack
     make clean &>/dev/null
     echo -e "Checking your image.gz-dtb..."
     sleep 3
-      if ! [ -e $kernel_img ]; then
+      if ! [ -f $kernel_img ]; then
           echo -e "Image.gz-dtb Not Found!"
           sleep 1
           echo -e "Aborting process..."
@@ -96,6 +104,7 @@ if [ "$choice" = "3" ]; then
     cd ..
 fi
 if [ "$choice" = "4" ]; then
+    clear
     exit 
 fi
 done
