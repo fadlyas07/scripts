14555b281ccf1cb1a826348fd0875e9add92ff0c
fadlyas07 <fadlyardhians@gmail.com>
2020-07-21 12:30:41 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -7,14 +7,10 @@
 # clone all preparations if not exist
 [[ ! -d "$(pwd)/anykernel-3" ]] && git clone https://github.com/fadlyas07/anykernel-3 --depth=1
 [[ ! -d "$(pwd)/telegram" ]] && git clone https://github.com/fabianonline/telegram.sh --depth=1 telegram
-if [[ "$(git rev-parse --abbrev-ref HEAD)" != "lineage-17.1" ]]; then
-    [[ ! -d "$(pwd)/tc-clang" ]] && git clone https://github.com/GreenForce-project-repository/clang-11.0.0 --depth=1 -b master llvm_clang
-else
-    [[ ! -d "$(pwd)/tc-gcc" ]] && git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth=1 -b lineage-17.1 origin_gcc
-    [[ ! -d "$(pwd)/tc-gcc32" ]] && git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth=1 -b lineage-17.1 origin_gcc32
-fi
+[[ ! -d "$(pwd)/tc-gcc" ]] && git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth=1 -b lineage-17.1 origin_gcc
+[[ ! -d "$(pwd)/tc-gcc32" ]] && git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth=1 -b lineage-17.1 origin_gcc32
 
-# let see `defconfig` to determine device for compiled
+# let see defconfig to determine device for compiled
 config_path="$(pwd)/arch/arm64/configs"
 if [[ -e "$config_path/ugglite_defconfig" ]]; then
     device="Xiaomi Redmi Note 5A Lite" && config_device1=ugglite_defconfig
@@ -26,17 +22,17 @@ fi
 git config --global user.name "fadlyas07"
 git config --global user.email "fadlyardhians@gmail.com"
 
-# create `Temporary` folder
+# create Temporary folder
 mkdir $(pwd)/temporary
 temp=$(pwd)/temporary
 
-# export build `Architecture`
+# export build Architecture
 export ARCH=arm64
 export SUBARCH=arm64
 
-# export `Telegram` Environment
+# export Telegram Environment
 export TELEGRAM_TOKEN="$token"
-# change chat_id to my_id for `A10` branch
+# change chat_id to my_id for any branch
 case "$(git rev-parse --abbrev-ref HEAD)" in
     "lineage-17.1")
         unset chat_id
@@ -44,13 +40,6 @@ case "$(git rev-parse --abbrev-ref HEAD)" in
     ;;
 esac
 export TELEGRAM_ID="$chat_id"
-
-# cchace inflation
-ccache -M 50G
-export USE_CCACHE=1
-export CCACHE_COMPRESS=1
-export WITHOUT_CHECK_API=true
-export CCACHE_EXEC=/usr/bin/ccache
 
 # some other kernel stuff
 pack=$(pwd)/anykernel-3
@@ -93,30 +82,26 @@ case "$(git rev-parse --abbrev-ref HEAD)" in
                                KBUILD_BUILD_HOST="$(TZ=Asia/Jakarta date +'%B')-build"
         }
         ;;
-    *)
+       *)
         make_kernel()
         {
-         export LD_LIBRARY_PATH=$(pwd)/llvm_clang/lib:"$PATH"
-         PATH=$(pwd)/llvm_clang/bin:"$PATH" \
+         export LD_LIBRARY_PATH=/root/sdclang/lib:"$PATH"
+         PATH=$(pwd)/origin_gcc/bin:/root/sdclang/bin:$(pwd)/origin_gcc32/bin:"$PATH" \
          make -j$(nproc --all) O="out" \
                                ARCH="arm64" \
-                               AR="llvm-ar" \
                                CC="clang" \
-                               CROSS_COMPILE="aarch64-linux-gnu-" \
-                               CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+                               CLANG_TRIPLE="aarch64-linux-gnu-" \
+                               CROSS_COMPILE="aarch64-linux-android-" \
+                               CROSS_COMPILE_ARM32="arm-linux-androideabi-" \
                                KBUILD_BUILD_USER="Mhmmdfdlyas" \
-                               KBUILD_BUILD_HOST="$(TZ=Asia/Jakarta date +'%B')-build" \
-                               NM="llvm-nm" \
-                               OBJCOPY="llvm-objcopy" \
-                               OBJDUMP="llvm-objdump" \
-                               STRIP="llvm-strip"
+                               KBUILD_BUILD_HOST="$(TZ=Asia/Jakarta date +'%B')-build"
         }
         ;;
 esac
 
-# `time` mark started build
+# time mark started build
 build_start=$(date +"%s")
-# `time` mark build (1)
+# time mark build (1)
 date1=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
 
 # build kernel
@@ -125,18 +110,19 @@ make_kernel 2>&1| tee Log-$(TZ=Asia/Jakarta date +'%d%m%y').log
 mv Log-*.log "$temp"
 # find errors
 if ! [[ -f "$kernel_img" ]]; then
-    # `time` mark errors build
+    # time mark errors build
     build_end=$(date +"%s")
     build_diff=$(($build_end - $build_start))
-    # ship log to `del.dog` & grep errors to `trimmed_log.txt`
+    # ship log to del.dog & grep errors to trimmed_log.txt
     grep -iE 'not|empty|in file|waiting|crash|error|fail|fatal' "$(echo $temp/*.log)" &> "$temp/trimmed_log.txt"
     send_to_dogbin=$(echo https://del.dog/$(jq -r .key <<< $(curl -sf --data-binary "$(cat $(echo $temp/*.log))" https://del.dog/documents)))
     # ship it all to main channel
     curl -F document=@$(echo $temp/*.log) "https://api.telegram.org/bot"$TELEGRAM_TOKEN"/sendDocument" -F chat_id="784548477"
     curl -F document=@$(echo $temp/*.txt) "https://api.telegram.org/bot"$TELEGRAM_TOKEN"/sendDocument" -F chat_id="$TELEGRAM_ID" -F caption="$send_to_dogbin"
-    tg_send_message "<b>$product_name</b> for <b>$device</b> on branch '<b>$(git rev-parse --abbrev-ref HEAD)</b>' Build errors in $(($build_diff / 60)) minutes and $(($build_diff % 60)) seconds."
+    tg_send_message "<b>$product_name</b> for <b>$device</b> on branch '<b>$(git rev-parse --abbrev-ref HEAD)</b>' Build errors in <b>$(($build_diff / 60)) minutes</b> and <b>$(($build_diff % 60)) seconds</b>."
     exit 1
 fi
+# ship success log to private chat
 curl -F document=@$(echo $temp/*.log) "https://api.telegram.org/bot"$TELEGRAM_TOKEN"/sendDocument" -F chat_id="784548477"
 mv "$kernel_img" "$pack/zImage" && cd $pack
 if [[ "$device" = "Xiaomi Redmi Note 5A Lite" ]]; then
@@ -151,13 +137,14 @@ if [[ "$device" != "Xiaomi Redmi Note 5A Lite" ]]; then
 # delete output, previous log, and zImage
 rm -rf out $temp/*.log $pack/zImage
 
-# `time` mark build (2)
+# time mark build (2)
 date2=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
 
 # build kernel
 make ARCH=arm64 O=out $config_device2
 make_kernel 2>&1| tee Log-$(TZ=Asia/Jakarta date +'%d%m%y').log
 mv Log-*.log "$temp"
+# ship success log to private chat
 curl -F document=@$(echo $temp/*.log) "https://api.telegram.org/bot"$TELEGRAM_TOKEN"/sendDocument" -F chat_id="784548477"
 mv "$kernel_img" "$pack/zImage" && cd $pack
 if [[ "$device" = "Xiaomi Redmi 4A/5A" ]]; then
@@ -166,7 +153,7 @@ fi
 cd ..
 fi
 
-# `time` mark ended build
+# time mark ended build
 build_end=$(date +"%s")
 build_diff=$(($build_end - $build_start))
 
