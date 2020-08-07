819d1217b4a0b162d4bef257a6b95041c53f7c60
Mhmmdfdlyas! <fadlyardhians@gmail.com>
2020-08-07 07:08:38 +0700

    kernel.sh: Use GCC & cleanup script
    rewrite kernel.sh (69%)

--- kernel.sh
+++ kernel.sh
@@ -6,65 +6,32 @@
 if [[ -n $CI ]] ; then
     echo "Yeay, build running on CI!" ;
         if [[ -z $chat_id ]] && [[ -z $token ]] ; then
-            echo 'chat id and token does not exist!' ;
+            echo 'chat id and bot token does not exist!' ;
             exit 1 ;
         fi
     ls -Aq &>/dev/null
 else
     echo "Okay, build running on my VM" ;
         if [[ -z $chat_id ]] && [[ -z $token ]] ; then
-            read -p "Enter your chat_id: " chat_id
+            read -p "Enter your chat id: " chat_id
             read -p "Enter your bot token: " token
+            export chat_id token
         fi
     ls -Aq &>/dev/null
 fi
+case '$(git rev-parse --abbrev-ref HEAD)' in *"17.1"*)
+unset chat_id && export chat_id="784548477" ;; esac
 
 config_path="$(pwd)/arch/arm64/configs"
 if [[ -e "$config_path/ugglite_defconfig" ]] ; then
-    device="Xiaomi Redmi Note 5A Lite" && config_device1=ugglite_defconfig
+    device=ugglite && config_device1=ugglite_defconfig
 elif [[ -e "$config_path/rolex_defconfig" ]] || [[ -e "$config_path/riva_defconfig" ]] ; then
-    device="Xiaomi Redmi 4A/5A" && config_device1=rolex_defconfig && config_device2=riva_defconfig
+    device=rova && config_device1=rolex_defconfig && config_device2=riva_defconfig
 fi
 
-case "$(git rev-parse --abbrev-ref HEAD)" in
-lineage-17.1)
-        unset chat_id && export chat_id="784548477"
-        clone_toolchain() {
-        [[ ! -d "$(pwd)/origin_gcc" ]] && git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth=1 -b lineage-17.1 origin_gcc
-        [[ ! -d "$(pwd)/origin_gcc32" ]] && git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth=1 -b lineage-17.1 origin_gcc32
-        }
-        build_kernel() {
-        PATH="$(pwd)/origin_gcc/bin:$(pwd)/origin_gcc32/bin:$PATH" \
-        make "-j$(nproc --all)" O=out \
-                                ARCH=arm64 \
-                                CROSS_COMPILE=aarch64-linux-android- \
-                                CROSS_COMPILE_ARM32=arm-linux-androideabi-
-        }
-        ;;
-*)
-        clone_toolchain() {
-        [[ ! -d "$(pwd)/llvm_clang" ]] && git clone https://github.com/GreenForce-project-repository/android_vendor_qcom_proprietary_llvm-arm-toolchain-ship_8.0.6 --depth=1 -b 10.0 llvm_clang
-        [[ ! -d "$(pwd)/origin_gcc" ]] && git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 -b android-9.0.0_r58 origin_gcc
-        [[ ! -d "$(pwd)/origin_gcc32" ]] && git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 --depth=1 -b android-9.0.0_r58 origin_gcc32
-        }
-        build_kernel() {
-        export CCV="$($(pwd)/llvm_clang/bin/clang --version | head -n1 | perl -pe 's/\(.*?\)//gs' | sed 's/[[:space:]]*$//')" ;
-        export LDV="$($(pwd)/llvm_clang/bin/ld.lld --version | head -n1 | perl -pe 's/\(.*?\)//gs' | sed 's/(compatible with [^)]*)//' | sed 's/[[:space:]]*$//')" ;
-        export LD_LIBRARY_PATH="$(pwd)/llvm_clang/lib:$LD_LIBRARY_PATH" ;
-        PATH="$(pwd)/llvm_clang/bin:$PATH" \
-        make "-j$(nproc --all)" O=out \
-                                ARCH=arm64 \
-                                CC=clang \
-                                CLANG_TRIPLE=aarch64-linux-gnu- \
-                                CROSS_COMPILE=aarch64-linux-android- \
-                                CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-                                KBUILD_COMPILER_STRING="$CCV, $LDV"
-        }
-        ;;
-esac
-
-clone_toolchain &>/dev/null
-[[ ! -d "$(pwd)/anykernel-3" ]] && git clone https://github.com/fadlyas07/anykernel-3 --depth=1
+[[ ! -d "$(pwd)/anykernel-3" ]] && git clone https://github.com/fadlyas07/anykernel-3 --depth=1 &>/dev/null
+[[ ! -d "$(pwd)/origin_gcc" ]] && git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 -b android-9.0.0_r58 origin_gcc &>/dev/null
+[[ ! -d "$(pwd)/origin_gcc32" ]] && git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 --depth=1 -b android-9.0.0_r58 origin_gcc32 &>/dev/null
 
 # Needed to export
 export ARCH=arm64
@@ -76,12 +43,6 @@ export KBUILD_BUILD_HOST=circleci-Lab
 
 mkdir "$(pwd)/temporary"
 
-tg_send_sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
-         -d sticker="CAACAgUAAxkBAAEYl9pee0jBz-DdWSsy7Rik8lwWE6LARwACmQEAAn1Cwy4FwzpKLPPhXRgE" \
-         -d chat_id="$TELEGRAM_ID"
-}
-
 tg_send_message() {
     curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
          -d "disable_web_page_preview=true" \
@@ -91,7 +52,15 @@ tg_send_message() {
                     for POST in "${@}" ; do
                         echo "$POST"
                     done
-            )"
+            )" &>/dev/null
+}
+
+build_kernel() {
+    PATH="$(pwd)/origin_gcc/bin:$(pwd)/origin_gcc32/bin:$PATH" \
+    make "-j$(nproc --all)" O=out \
+                            ARCH=arm64 \
+                            CROSS_COMPILE=aarch64-linux-android- \
+                            CROSS_COMPILE_ARM32=arm-linux-androideabi-
 }
 
 # Main Environment
@@ -100,70 +69,54 @@ temp="$(pwd)/temporary"
 pack="$(pwd)/anykernel-3"
 kernel_img="$(pwd)/out/arch/arm64/boot/Image.gz-dtb"
 
-# build kernel
-build_start=$(date +"%s")
+# build kernel - 1
+build_start1=$(date +"%s")
 build_date1="$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')"
 
 make ARCH=arm64 O=out $config_device1 &>/dev/null
 build_kernel 2>&1| tee "Log-$(TZ=Asia/Jakarta date +'%d%m%y').log"
-mv Log-*.log "$temp"
+mv Log-*.log $temp
 
-if [[ ! -f $kernel_img ]] ; then
+if [[ ! -f "$kernel_img" ]] ; then
     build_end=$(date +"%s")
-    build_diff=$(($build_end - $build_start))
-    grep -iE 'Stop|not|empty|in file|waiting|crash|error|fail|fatal' "$(echo $temp/Log-*.log)" &> "$temp/trimmed_log.txt"
-    send_to_dogbin="$(echo https://del.dog/raw/$(jq -r .key <<< $(curl -sf --data-binary "$(cat $temp/Log-*.log)" https://del.dog/documents)))"
+    build_diff=$(($build_end - $build_start1))
+    grep -iE 'Stop|not|empty|in file|waiting|crash|error|fail|fatal' $(echo $temp/Log-*.log) &> "$temp/trimmed_log.txt"
+    send_to_dogbin="$(echo https://del.dog/raw/$(jq -r .key <<< $(curl -sf --data-binary '$(cat $temp/Log-*.log)' https://del.dog/documents)))"
     curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
-    curl -F document=@$(echo $temp/*.txt) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
-    tg_send_message "<b>build throw an errors!</b>" \
-                    "<b>Branch :</b> origin/$(git rev-parse --abbrev-ref HEAD) ($(git rev-parse --abbrev-ref HEAD | cut -b 9-15))" \
-                    "<b>Log :</b> $send_to_dogbin" \
-                    "<b>Time elapsed :</b> $(($build_diff / 60)) minutes, $(($build_diff % 60)) seconds."
+    curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID" -F "disable_web_page_preview=true" -F "parse_mode=html" -F caption="<b>build throw an errors!</b> ($(git rev-parse --abbrev-ref HEAD | cut -b 9-15)) (Log : $send_to_dogbin) Build took $(($build_diff / 60)) minutes, $(($build_diff % 60)) seconds."
     exit 1 ;
 fi
 
+build_end1=$(date +"%s")
+build_diff1=$(($build_end1 - $build_start1))
+kernel_version="$(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3)"
 curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
 mv "$kernel_img" "$pack/zImage" && cd $pack
-if [[ $device == "Xiaomi Redmi Note 5A Lite" ]] ; then
-    zip -r9q $product_name-ugglite-"$build_date1".zip * -x .git README.md LICENCE $(echo *.zip)
-elif [[ $device == "Xiaomi Redmi 4A/5A" ]] ; then
-    zip -r9q $product_name-rolex-"$build_date1".zip * -x .git README.md LICENCE $(echo *.zip)
+if [[ "$device" == "ugglite" ]] ; then
+    zip -r9 $product_name-ugglite-"$build_date1".zip * -x .git README.md LICENCE $(echo *.zip) &>/dev/null && cd ..
+    curl -F document=@$(echo $pack/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID" -F caption="New #ugglite build is available! ($kernel_version, $(git rev-parse --abbrev-ref HEAD | cut -b 9-15)) at commit $(git log --pretty=format:"%h (\"%s\")" -1) Build took $(($build_diff1 / 60)) minutes, $(($build_diff1 % 60)) seconds."
+elif [[ "$device" == "rova" ]] ; then
+    zip -r9 $product_name-rolex-"$build_date1".zip * -x .git README.md LICENCE $(echo *.zip) &>/dev/null && cd ..
+    curl -F document=@$(echo $pack/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID" -F caption="New #rolex build is available! ($kernel_version, $(git rev-parse --abbrev-ref HEAD | cut -b 9-15)) at commit $(git log --pretty=format:"%h (\"%s\")" -1) Build took $(($build_diff1 / 60)) minutes, $(($build_diff1 % 60)) seconds."
 fi
-cd ..
 
-if [[ $device != "Xiaomi Redmi Note 5A Lite" ]] ; then
-    rm -rf out "$temp/Log-*.log" "$pack/zImage"
+if [[ "$device" != "ugglite" ]] ; then
+    rm -rf out $pack/*.zip $temp/Log-*.log $pack/zImage
 
-    # build kernel
+    # build kernel - 2
+    build_start2=$(date +"%s")
     build_date2="$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')"
     make ARCH=arm64 O=out $config_device2 &>/dev/null
     build_kernel 2>&1| tee "Log-$(TZ=Asia/Jakarta date +'%d%m%y').log"
-    mv Log-*.log "$temp"
+    mv Log-*.log $temp
 
+    build_end2=$(date +"%s")
+    build_diff2=$(($build_end2 - $build_start2))
+    kernel_version="$(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3)"
     curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
     mv "$kernel_img" "$pack/zImage" && cd $pack
-        if [[ $device == "Xiaomi Redmi 4A/5A" ]] ; then
-            zip -r9q $product_name-riva-"$build_date2".zip * -x .git README.md LICENCE $(echo *.zip)
-        fi
-    cd ..
-fi
-
-build_end=$(date +"%s")
-build_diff=$(($build_end - $build_start))
-kernel_version=$(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3)
-toolchain_version=$(cat $(pwd)/out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
-
-tg_send_sticker
-tg_send_message "<b>Device :</b> $device" \
-                "<b>Branch :</b> origin/$(git rev-parse --abbrev-ref HEAD) ($(git rev-parse --abbrev-ref HEAD | cut -b 9-15))" \
-                "<b>Kernel version :</b> Linux $kernel_version" \
-                "<b>Compiler :</b> $toolchain_version" \
-                "<b>Latest commit :</b> $(git log --pretty=format:'%s' -1)" \
-                "<b>Time elapsed :</b> $(($build_diff / 60)) minutes, $(($build_diff % 60)) seconds."
-if [[ $device == "Xiaomi Redmi Note 5A Lite" ]] ; then
-    curl -F document=@$(echo $pack/$product_name-ugglite-$build_date1.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
-elif [[ $device == "Xiaomi Redmi 4A/5A" ]] ; then
-    curl -F document=@$(echo $pack/$product_name-rolex-$build_date1.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
-    sleep 2
-    curl -F document=@$(echo $pack/$product_name-riva-$build_date2.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
+    if [[ "$device" == "rova" ]] ; then
+        zip -r9 $product_name-riva-"$build_date2".zip * -x .git README.md LICENCE $(echo *.zip) &>/dev/null && cd ..
+        curl -F document=@$(echo $pack/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID" -F caption="New #riva build is available! ($kernel_version, $(git rev-parse --abbrev-ref HEAD | cut -b 9-15)) at commit $(git log --pretty=format:"%h (\"%s\")" -1) Build took $(($build_diff2 / 60)) minutes, $(($build_diff2 % 60)) seconds."
+    fi
 fi
