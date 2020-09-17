1f55c3dafcdccf2b8a1515340313ea45709a0d30
Mhmmdfdlyas! <fadlyardhians@gmail.com>
2020-09-17 15:32:55 +0700

    kernel/personal/secure: cleanup!

--- kernel/personal/secure.build.sh
+++ kernel/personal/secure.build.sh
@@ -10,8 +10,8 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 [[ ! -d "$(pwd)/anykernel-3" ]] && git clone https://github.com/fadlyas07/anykernel-3 --depth=1
-[[ ! -d "$(pwd)/gcc" ]] && git clone https://github.com/arter97/arm64-gcc --depth=1 -b master gcc &>/dev/null
-[[ ! -d "$(pwd)/gcc32" ]] && git clone https://github.com/arter97/arm32-gcc --depth=1 -b master gcc32 &>/dev/null
+[[ ! -d "$(pwd)/gcc" ]] && git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 -b android-9.0.0_r45 gcc &>/dev/null
+[[ ! -d "$(pwd)/gcc32" ]] && git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 --depth=1 -b android-9.0.0_r45 gcc32 &>/dev/null
 
 # Main Environment
 codename="$1"
@@ -40,14 +40,14 @@ export TELEGRAM_TOKEN="$5"
 export TELEGRAM_PRIV="$4"
 export KBUILD_BUILD_USER=fadlyas07
 export KBUILD_BUILD_HOST=circleci-Lab
+export KBUILD_BUILD_TIMESTAMP=$(TZ=Asia/Jakarta date)
 
-echo Build started ... ;
 build_date="$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')"
 make ARCH=arm64 O=out "$3" &>/dev/null
 PATH="$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH" \
 make -j"$(nproc --all)" -l"$(nproc --all)" O=out \
-ARCH=arm64 CROSS_COMPILE=aarch64-elf- \
-CROSS_COMPILE_ARM32=arm-eabi- 2>&1| tee "Log-$(TZ=Asia/Jakarta date +'%d%m%y').log"
+ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- \
+CROSS_COMPILE_ARM32=arm-linux-androideabi- 2>&1| tee "Log-$(TZ=Asia/Jakarta date +'%d%m%y').log"
 mv Log-*.log "$temp"
 
 if [[ ! -f "$kernel_img" ]] ; then
@@ -58,20 +58,17 @@ else
     kernel_version="$(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3)"
     curl -F document=@$(echo $temp/Log-*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_PRIV"
     mv "$kernel_img" "$pack/zImage" && cd $pack || exit 1 ;
-    zip -r9 $product_name-$codename-"$build_date".zip * -x .git README.md LICENCE $(echo *.zip) &>/dev/null && cd .. || exit 1 ;
+    zip -r9q $product_name-$codename-"$build_date".zip * -x .git README.md LICENCE $(echo *.zip) &>/dev/null && cd .. || exit 1 ;
     curl -F chat_id="$TELEGRAM_ID" -F caption="New #$codename build is available! ($kernel_version, $(git rev-parse --abbrev-ref HEAD)) at commit $(git log --pretty=format:"%h (\"%s\")" -1)" \
          -F "disable_web_page_preview=true" -F "parse_mode=html" \
          -F document=@$(echo $pack/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument"
 fi
 
 case "$6" in
-    "yes|Yes|yEs|yeS|YES")
-        rm -rf out $(echo $pack/*.zip) $pack/zImage $temp/$(echo *.log)
-        ;;
-    "no|No|nO|NO")
-        rm -rf $temp/$(echo *.log)
-        ;;
+    yes)
+        rm -rf out $(echo $pack/*.zip) $pack/zImage $temp/$(echo *.log) ;;
+    no)
+        rm -rf $temp/$(echo *.log) ;;
     *)
-        echo "nothing to do, kek!"
-        ;;
+        echo "nothing to do, kek!" ;;
 esac
