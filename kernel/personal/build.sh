511396eee92e609107eb63d51f33e33f69c11f40
fadlyas07 <mhmmdfdlyas@proton.me>
2024-04-19 11:07:19 +0700

    kernel: personal: Sync my new personal usage script
    Change-Id: I277f3334bf2584e1b90886cf4876e9269a874196

--- kernel/personal/build.sh
+++ kernel/personal/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Lite kernel compilation script [ with Args ]
-# Copyright (C) 2020, 2022, 2023 Muhammad Fadlyas (fadlyas07)
+# Simple Kernel Build Script
+# Copyright (C) 2020-2024 Muhammad Fadlyas (fadlyas07)
 # SPDX-License-Identifier: GPL-3.0-or-later
 export DIR="$(pwd)"
 if [[ $# -eq 0 ]]; then
@@ -11,50 +11,52 @@ if ! [[ -f "${DIR}/Makefile" && -d "${DIR}/kernel" ]]; then
     echo "Please run this script inside kernel source folder!"
     exit 1
 fi
-echo "Cloning dependencies..."
+echo "Cloning toolchain dependencies..."
+wget https://github.com/greenforce-project/greenforce_clang/raw/main/latest_url.txt && source latest_url.txt
 [[ ! -d "${DIR}/AnyKernel3" ]] && git clone --single-branch https://github.com/greenforce-project/AnyKernel3 --depth=1 &>/dev/null
-[[ ! -d "${DIR}/aosp_clang" ]] && git clone --single-branch https://github.com/greenforce-project/aosp_clang --depth=1 &>/dev/null
-[[ ! -d "${DIR}/aarch64-linux-android-4.9" ]] && git clone --single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r40 --depth=1 &>/dev/null
-[[ ! -d "${DIR}/arm-linux-androideabi-4.9" ]] && git clone --single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r40 --depth=1 &>/dev/null
+[[ ! -d "${DIR}/greenforce_clang" ]] && mkdir -p "${DIR}/greenforce_clang" &&
+    wget -c "$latest_url" -O - | tar --use-compress-program=unzstd -xf - -C "${DIR}/greenforce_clang" &>/dev/null
+[[ ! -d "${DIR}/gcc-arm64" ]] && git clone --single-branch https://github.com/greenforce-project/gcc-arm64.git -b main --depth=1 &>/dev/null
+[[ ! -d "${DIR}/gcc-arm" ]] && git clone --single-branch https://github.com/greenforce-project/gcc-arm.git -b main --depth=1 &>/dev/null
 echo "All dependencies cloned!"
 export ARCH=arm64
-export SUBARCH="${ARCH}"
-export KBUILD_BUILD_USER=personal
-export KBUILD_BUILD_HOST=greenforce-project
-export kernel_branch=$(git rev-parse --abbrev-ref HEAD)
 export TZ=Asia/Jakarta
+export KBUILD_BUILD_USER=MuhammadFadlyArdhianS
+export KBUILD_BUILD_HOST=personal-build
+export kernel_branch="$(git rev-parse --abbrev-ref HEAD)"
 source <(grep -E '^(VERSION|PATCHLEVEL)' Makefile | sed -e s/[[:space:]]//g)
 if ! [[ -n "$VERSION" && -n "$PATCHLEVEL" ]]; then
     echo "Unable to get kernel version from Makefile!"
     exit 1
 fi
-export codename="${2}" &>/dev/codename
-if [[ -n "${codename}" ]]; then
-    if [[ -f "/dev/codename" ]]; then
-        export codename="$(cat /dev/codename)"
-    else
-        echo "codename is empty, please run 'export codename=[CODENAME]'"
-        exit 1
-    fi
+export codename="${2}"
+if [[ -z "${codename}" ]]; then
+    echo "codename is empty, please provide a codename."
+    exit 1
 fi
 export kernelversion="$VERSION.$PATCHLEVEL"
-case "${kernelversion}" in
-    4.19|5.4|5.10|5.15|6.0)
-        export defconfig="vendor/${codename}-perf_defconfig"
-        export vdso_flags='CROSS_COMPILE_COMPAT'
-        ;;
-    *)
-        export defconfig="${codename}-perf_defconfig"
-        export vdso_flags='CROSS_COMPILE_ARM32'
-        ;;
-esac
+export defconfig="vendor/${codename}-perf_defconfig"
+for version in 3.18 4.4 4.9 4.14; do
+    if [[ "$version" == "${kernelversion}" ]]; then
+        export vdso_flags="CROSS_COMPILE_COMPAT"
+        case "${kernelversion}" in
+            3.18 | 4.4 | 4.9)
+                export defconfig="${codename}-perf_defconfig"
+                export vdso_flags="CROSS_COMPILE_ARM32"
+                ;;
+        esac
+    fi
+done
 export PATH="${DIR}/aosp_clang/bin:${DIR}/aarch64-linux-android-4.9/bin:${DIR}/arm-linux-androideabi-4.9/bin:${PATH}"
-export LD_LIBRARY_PATH="${DIR}/aosp_clang/bin/../lib:${LD_LIBRARY_PATH}"
-export IMG_PATH="${DIR/out/arch/$ARCH/boot}"
-build_flags="ARCH=$ARCH CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- ${vdso_flags}=arm-linux-androideabi- "
+export IMG_PATH="${DIR}/out/arch/$ARCH/boot"
+build_flags="ARCH=$ARCH CC=clang CROSS_COMPILE=aarch64-linux-gnu- ${vdso_flags}=arm-linux-gnueabi- "
 build_flags+="AR=llvm-ar OBJDUMP=llvm-objdump STRIP=llvm-strip NM=llvm-nm "
 [[ "${3}" == llvm || "${3}" == full ]] && build_flags+="LLVM=1 "
-make -j$(nproc --all) -C ${DIR} O=out ${build_flags} "${defconfig}" 2>&1| build.log
+echo "Regenerating ${defconfig}..."
+make -j$(nproc --all) -l$(nproc --all) -C "${DIR}" O=out ${build_flags} "${defconfig}" 2>&1 | build.log
+echo "Regenerating ${defconfig}... done!"
+echo "Build kernel started..."
+make -j$(nproc --all) -l$(nproc --all) -C "${DIR}" O=out ${build_flags} 2>&1 | build.log
 if ! [[ -f "${IMG_PATH}/Image.gz-dtb" || -f "${IMG_PATH}/Image.gz" ]]; then
     echo "Build failed, please check build log and fix it!"
     exit 1
@@ -62,13 +64,13 @@ else
     echo "Build Complete, find kernel image in ${IMG_PATH}"
 fi
 anykernel_string="${kernel_branch} - $(date +'%A'), $(date +'%d %B %Y')"
-sed -i "s/kernel.string=/kernel.string=${anykernel_string}/g" ${DIR}/AnyKernel3/anykernel.sh
-sed -i "s/device.name1=/device.name1=${codename}/g" ${DIR}/AnyKernel3/anykernel.sh
+sed -i "s/kernel.string=/kernel.string=${anykernel_string}/g" "${DIR}/AnyKernel3/anykernel.sh"
+sed -i "s/device.name1=/device.name1=${codename}/g" "${DIR}/AnyKernel3/anykernel.sh"
 [[ -e "${IMG_PATH}/Image.gz-dtb" ]] && mv "${IMG_PATH}/Image.gz-dtb" "${DIR}/AnyKernel3"
-[[ -e "${IMG_PATH}/Image.gz" ]] && mv "${IMG_PATH/Image.gz" "${DIR}/AnyKernel3"
+[[ -e "${IMG_PATH}/Image.gz" ]] && mv "${IMG_PATH}/Image.gz" "${DIR}/AnyKernel3"
 [[ -e "${IMG_PATH}/dtb.img" ]] && mv "${IMG_PATH}/dtb.img" "${DIR}/AnyKernel3"
 [[ -e "${IMG_PATH}/dtb.img" ]] && mv "${IMG_PATH}/dtbo.img" "${DIR}/AnyKernel3"
 echo "Compress flashable kernel..."
-zip -r9 "GF~${KBUILD_BUILD_USER}-${codename}-$(date +'%d%m%y').zip" $DIR/AnyKernel3/*
+zip -r9 "GF~${KBUILD_BUILD_USER}-${codename}-$(date +'%d%m%y').zip" "${DIR}/AnyKernel3/*"
 echo "complete!"
-echo "Build complete! find kernel zip in $(find $DIR/AnyKernel3/*.zip)."
+echo "Build complete! find kernel zip in $(find "${DIR}/AnyKernel3"/*.zip)."
